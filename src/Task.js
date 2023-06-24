27b33033f8ab7f277c81a675368bd4a42ca2fcf2
Viky_pebriyanto <Viky12345697@gmail.com>
2023-06-24 15:11:04 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "vikypebriyanto-fe5599426.netlify.app" // TODO: replace this
+const studentName = "Viky Pebriyanto" // TODO: replace this
+const studentId = "FE5599426" // TODO: replace this
+
+export { url, studentName, studentId }
